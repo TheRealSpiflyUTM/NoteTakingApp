a413054ff60facf9b72fb26c651ab5b979397f66
TheRealSpiflyUTM <ion.novac@isa.utm.md>
2026-09-18 19:14:36 +0300

    Initialize backEnd and FrontEnd React.js and SpringBoot

---
+++ backend/src/main/java/com/notetakingapp/backend/HelloController.java
@@ -0,0 +1,12 @@
+package com.notetakingapp.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HelloController {
+    @GetMapping ("/api/hello")
+    public String hello(){
+        return "Spring boot is working chicri briki";
+    }
+}
